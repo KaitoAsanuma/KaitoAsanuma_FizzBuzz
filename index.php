b7a57be54c5dd1b424fab8140cc0b8d4d3910e1a
Kaito Asanuma <sublimaton0730@gmail.com>
2022-05-13 17:15:11 +0900

    FizzBuzz

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>FizzBuzz</title>
-</head>
-<body>
-  <?php
+<?php
    for ($i = 1; $i <= 100; $i++) {
     if ($i % 15 == 0) {
-      echo 'FizzBuzz';
-      echo '<br/>';
+      echo 'FizzBuzz'.'<br/>';
     } else if ($i % 3 == 0) {
-      echo 'Fizz';
-      echo '<br/>';
+      echo 'Fizz'.'<br/>';
     } else if ($i % 5 == 0) {
-      echo 'Buzz';
-      echo '<br/>';
+      echo 'Buzz'.'<br/>';
     } else {
-      echo $i;
-      echo '<br/>';
+      echo $i.'<br/>';
     }
    }
-  ?>
-</body>
-</html>
+?>
